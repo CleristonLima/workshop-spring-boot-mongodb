0718779d4a917d8e3254afaa13d50ba71f5e3764
Cleriston Lima <cleriston-santos2012@hotmail.com>
2024-08-08 11:48:07 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/cleristonlima/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleristonlima.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
