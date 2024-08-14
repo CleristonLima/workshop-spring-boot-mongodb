b374448d51177f267e4b00c6174132dfda1dd95e
Cleriston Lima <cleriston-santos2012@hotmail.com>
2024-08-14 12:14:30 -0300

    Inserção de usuário com POST

--- src/main/java/com/cleristonlima/workshopmongo/services/UserService.java
+++ src/main/java/com/cleristonlima/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cleristonlima.workshopmongo.domain.User;
+import com.cleristonlima.workshopmongo.dto.UserDTO;
 import com.cleristonlima.workshopmongo.repository.UserRepository;
 import com.cleristonlima.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +27,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
