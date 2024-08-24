7f735be14c7d3cbd36ab9b587d9e6aebb5f851ca
Cleriston Lima <cleriston-santos2012@hotmail.com>
2024-08-24 12:25:36 -0300

    Consulta simples com query methods

--- src/main/java/com/cleristonlima/workshopmongo/resources/PostResource.java
+++ src/main/java/com/cleristonlima/workshopmongo/resources/PostResource.java
@@ -1,14 +1,17 @@
 package com.cleristonlima.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cleristonlima.workshopmongo.domain.Post;
-import com.cleristonlima.workshopmongo.dto.UserDTO;
+import com.cleristonlima.workshopmongo.resources.util.URL;
 import com.cleristonlima.workshopmongo.services.PostService;
 
 @RestController
@@ -24,5 +27,11 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
+		text = URL.descodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 
 }
